9bf49f547c48cb769efb7fccf05d526985e7b025
Andy Coupe <andrewmcoupe@gmail.com>
2020-08-08 08:45:28 +0100

    lint-staged check

--- errors/index.ts
+++ errors/index.ts
@@ -1,9 +1,9 @@
-import createError from "http-errors";
+import createError from 'http-errors'
 
 export const BadRequest = (errorMessage?: string) => {
-  return new createError.BadRequest(errorMessage);
-};
+  return new createError.BadRequest(errorMessage)
+}
 
 export const NotFound = (errorMessage?: string) => {
-  return new createError.NotFound(errorMessage);
-};
+  return new createError.NotFound(errorMessage)
+}
